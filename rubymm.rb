70c4593a7929e4b0cf54de066a76e4513947af98
Leonard Soai-Van <leo.soaivan@gmail.com>
2016-08-05 12:32:46 -0400

    Add #player_guess

--- rubymm.rb
+++ rubymm.rb
@@ -1,7 +1,11 @@
 #!/usr/bin/env ruby
 
 class Game
+  attr_accessor :code, :charset
+
   def initialize
+    @charset = ("A".."F").to_a
+    @code = []
     start_message
   end
 
@@ -9,7 +13,23 @@ class Game
 
   def start_message
     puts "Welcome to Master Mind!"
-    puts "Try to guess the computer's 4-character code, from A through F."
+    puts "Try to guess the computer's 4-character code, using the letters A through F."
+    puts "Duplicates letters ARE allowed!"
+    puts ""
+  end
+
+  def code_generator
+  	4.times do
+  	  @code << @charset[rand(6)]
+  	end
+  end
+
+  def player_guess
+  	begin
+  	  puts "Enter your 4-letter code:"
+  	  guess = gets.gsub!(/\s+/, "").upcase
+  	end until guess.size == 4 && /[A-F]+{4}/.match(guess)
+  	guess
   end
 end
 
